9a9f0e39e17144359058d1cf391827988da4a6fd
choetaeyang <imsun0101@naver.com>
2024-04-08 22:06:39 +0900

    [Feat]:프로젝트 초기 세팅

---
+++ src/test/java/doeat/doeat/DoeatApplicationTests.java
@@ -0,0 +1,13 @@
+package doeat.doeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
